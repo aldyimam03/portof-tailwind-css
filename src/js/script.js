69818c4fecbd59eaeaaa9da8d6b62891fec1fd3e
Aldy Imam Wijaya <aldyimam03@gmail.com>
2025-08-06 12:35:47 +0700

    merubah web portofolio

--- src/js/script.js
+++ src/js/script.js
@@ -18,3 +18,23 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
 });
+
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+  e.preventDefault(); // cegah submit default
+
+  let name = document.getElementById('name').value;
+  let email = document.getElementById('email').value;
+  let message = document.getElementById('message').value;
+
+  // Encode agar aman di URL
+  let subject = encodeURIComponent("Kontak dari Portfolio");
+  let body = encodeURIComponent(
+    `Nama: ${name}\nEmail: ${email}\nPesan: ${message}`
+  );
+
+  // Ganti dengan email kamu
+  let mailtoLink = `mailto:aldyimam02@gmail.com?subject=${subject}&body=${body}`;
+
+  // Buka di tab baru
+  window.location.href = mailtoLink;
+});
