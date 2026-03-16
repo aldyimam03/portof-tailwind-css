8412b4a9e9d6390a6fe9b264cbcc74c6a0115f53
Aldy Imam Wijaya <aldyimam03@gmail.com>
2026-03-16 21:28:48 +0700

    feat: add dark theme

--- src/js/script.js
+++ src/js/script.js
@@ -43,3 +43,31 @@ document.getElementById("contactForm").addEventListener("submit", function (e) {
     window.location.href = mailtoLink;
   }
 });
+
+// Dark mode toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+// Pindahkan posisi toggle sesuai mode
+// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  darkToggle.checked = true;
+  html.classList.add("dark");
+} else {
+  darkToggle.checked = false;
+  html.classList.remove("dark");
+}
